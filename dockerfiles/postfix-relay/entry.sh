65291a26a92fa8ddc749aa72e46892aa7d66736e
Matthew <draevich.matvey@gmail.com>
2022-07-01 15:11:07 +0300

    Initial commit

---
+++ dockerfiles/postfix-relay/entry.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+[ "${DEBUG:-false}" == 'true' ] && set -x
+
+# Defaults
+if [ -z "$MAILNAME" ]; then
+    echo "smtp >> Error: MAILNAME not specified"
+    exit 128
+fi
+
+if [ -z "$MYNETWORKS" ]; then
+    export MYNETWORKS='127.0.0.0/8, 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16'
+    echo "smtp >> Warning: MYNETWORKS not specified, allowing all private IPs"
+fi
+
+# Set timezone if set
+if [ ! -z "${TZ}" ]; then
+    echo "smtp >> Info: setting timezone to ${TZ}"
+    ln -snf "/usr/share/zoneinfo/${TZ}" /etc/localtime
+    echo "${TZ}" > /etc/timezone
+fi
+
+
+
+if [ ! -z "${RSPAMD}" ]; then
+    echo "smtp >> Info: setting rspamd server ($RSPAMD)"
+    postconf -e "milter_protocol = 6"
+    postconf -e "milter_default_action = accept"
+    postconf -e "smtpd_milters = inet:$RSPAMD"
+fi
+
+# Allow local customization scripts that run on every startup
+if [ -d /etc/entrypoint.d/ ]; then
+    /bin/run-parts -v /etc/entrypoint.d
+fi
+
+echo "Running command $*"
+exec "$@"
